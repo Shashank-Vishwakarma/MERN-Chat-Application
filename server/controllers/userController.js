035d51a1d88712e9801bdf5584d1c6ecf51fb563
Shashank <shashank78680@gmail.com>
2024-07-28 13:58:38 +0530

    route for getting all the users done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,5 @@
+import User from "../database/models/UserModel.js";
+
 export const getCurrentUser = (req, res) => {
     const currentUser = req.user;
     if(!currentUser) {
@@ -5,4 +7,13 @@ export const getCurrentUser = (req, res) => {
     }
 
     res.status(200).json({ currentUser });
+}
+
+export const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).json({ users });
+    } catch(err) {
+        res.status(500).json({ error: `Error in getting all users: ${err}` });
+    }
 }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getCurrentUser } from '../controllers/userController.js';
+import { getCurrentUser, getAllUsers } from '../controllers/userController.js';
 import { verifyJWTMiddleware } from '../middlewares/verifyJWTMiddleware.js';
 
 const userRoutes = express.Router();
 
 userRoutes.get('/current/profile', verifyJWTMiddleware, getCurrentUser);
+userRoutes.get('/', verifyJWTMiddleware, getAllUsers);
 
 export default userRoutes;
