d017622105785d5339f09f4baa18afb5020b85b1
Shashank <shashank78680@gmail.com>
2024-07-29 15:14:21 +0530

    added signup and login functionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const navigateTo = useNavigate();
+    const [inputs, setInputs] = useState({
+        username: "",
+        password: ""
+    });
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try { 
+            const response = await axios.post(
+                'http://localhost:3000/api/v1/auth/login',
+                inputs,
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    withCredentials: true
+                }
+            );
+
+            console.log(response.data);
+        } catch(error) {
+            console.log(`Error in Login Page: ${error}`);
+        }
+    }
 
     return (
         <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
             <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
                 <h1 className='text-3xl font-semibold text-center text-gray-300'>Login</h1>
 
-                <form className='flex flex-col justify-center gap-4 my-4'>
+                <form className='flex flex-col justify-center gap-4 my-4' onSubmit={handleLogin}>
                     <label className="input input-bordered flex items-center gap-2">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -19,7 +45,13 @@ const Login = () => {
                             <path
                                 d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
                         </svg>
-                        <input type="text" className="grow" placeholder="Username" />
+                        <input 
+                            type="text" 
+                            className="grow" 
+                            placeholder="Username" 
+                            value={inputs.username}
+                            onChange={(e)=> setInputs({...inputs, username: e.target.value})}
+                        />
                     </label>
 
                     <label className="input input-bordered flex items-center gap-2">
@@ -33,7 +65,13 @@ const Login = () => {
                                 d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
                                 clipRule="evenodd" />
                         </svg>
-                        <input type="password" className="grow" placeholder='Password' />
+                        <input 
+                            type="password" 
+                            className="grow" 
+                            placeholder="Password" 
+                            value={inputs.password}
+                            onChange={(e)=> setInputs({...inputs, password: e.target.value})}
+                        />
                     </label>
 
                     <div className='flex gap-4 justify-center'>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
     const navigateTo = useNavigate();
+    const [inputs, setInputs] = useState({
+        fullName: "",
+        username: "",
+        password: "",
+        confirmPassword: "",
+        gender: ""
+    });
+
+    const handleSignUp = async (e)=>{
+        e.preventDefault();
+        
+        try {
+            const response = await axios.post(
+                'http://localhost:3000/api/v1/auth/signup',
+                inputs,
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    withCredentials: true
+                }
+            );
+
+            console.log(response.data);
+        } catch(error) {
+            console.log(`Error in SignUp page: ${error}`);
+        }
+    }
 
     return (
         <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
             <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
                 <h1 className='text-3xl font-semibold text-center text-gray-300'>Sign Up</h1>
 
-                <form className='flex flex-col justify-center gap-4 my-4'>
+                <form 
+                    className='flex flex-col justify-center gap-4 my-4'
+                    onSubmit={handleSignUp}
+                >
                     <label className="input input-bordered flex items-center gap-2">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -19,7 +51,13 @@ const SignUp = () => {
                             <path
                                 d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
                         </svg>
-                        <input type="text" className="grow" placeholder="Full Name" />
+                        <input 
+                            type="text" 
+                            className="grow" 
+                            placeholder="Full Name" 
+                            value={inputs.fullName}
+                            onChange={(e)=> setInputs({...inputs, fullName: e.target.value})}
+                        />
                     </label>
 
                     <label className="input input-bordered flex items-center gap-2">
@@ -31,7 +69,13 @@ const SignUp = () => {
                             <path
                                 d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
                         </svg>
-                        <input type="text" className="grow" placeholder="Username" />
+                        <input 
+                            type="text" 
+                            className="grow" 
+                            placeholder="Username" 
+                            value={inputs.username}
+                            onChange={(e)=> setInputs({...inputs, username: e.target.value})}
+                        />
                     </label>
 
                     <label className="input input-bordered flex items-center gap-2">
@@ -45,7 +89,13 @@ const SignUp = () => {
                                 d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
                                 clipRule="evenodd" />
                         </svg>
-                        <input type="password" className="grow" placeholder='Password' />
+                        <input 
+                            type="password" 
+                            className="grow" 
+                            placeholder="Password" 
+                            value={inputs.password}
+                            onChange={(e)=> setInputs({...inputs, password: e.target.value})}
+                        />
                     </label>
 
                     <label className="input input-bordered flex items-center gap-2">
@@ -59,20 +109,30 @@ const SignUp = () => {
                                 d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
                                 clipRule="evenodd" />
                         </svg>
-                        <input type="password" className="grow" placeholder='Confirm Password' />
+                        <input 
+                            type="password" 
+                            className="grow" 
+                            placeholder="Confirm Password" 
+                            value={inputs.confirmPassword}
+                            onChange={(e)=> setInputs({...inputs, confirmPassword: e.target.value})}
+                        />
                     </label>
 
-                    <select className="select select-bordered select-md w-full">
-                        <option disabled selected>Select gender</option>
-                        <option>Male</option>
-                        <option>Female</option>
+                    <select 
+                        className="select select-bordered select-md w-full"
+                        value={inputs.gender}
+                        onChange={(e)=> setInputs({...inputs, gender: e.target.value})}
+                    >
+                        <option defaultValue>Select gender</option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
                     </select>
 
                     <div className='flex gap-4 justify-center'>
                         <span>Already have an account?</span>
                         <span onClick={() => navigateTo('/login')} className='font-bold cursor-pointer text-blue-400'>Login</span>
                     </div>
-                    <button type='submit' className='btn btn-block btn-sm mt-2' onClick={(e) => e.preventDefault()}>Sign Up</button>
+                    <button type='submit' className='btn btn-block btn-sm mt-2'>Sign Up</button>
                 </form>
             </div>
         </div>
